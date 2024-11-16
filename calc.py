4d33b3575cdeb752fc9eba3d7e6defb082ecd082
Abdala Atia <abdalaalaa17@gmail.com>
2024-11-16 22:47:24 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,14 +1,3 @@
-import unittest
-from Calc import Cal
-
-class TestCalculator(unittest.TestCase):
-
-    def test_add(self):
-        result = Cal.add(3, 7)
-        self.assertEqual(result, 10)
-        
-        result = Cal.add(-1, 1)
-        self.assertEqual(result, 0)
-        
-        result = Cal.add(-1, -1)
-        self.assertEqual(result, -2)
+class cal:
+    def add(a, b):
+        return a + b
